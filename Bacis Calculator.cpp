9528e06158699c74e293004508a6c9fc806369e4
Dipesh Maurya <149373441+Dipeshmaurya1@users.noreply.github.com>
2024-01-25 23:01:14 +0530

    Add files via upload

---
+++ Bacis Calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int a,b;
+	cout<<"Enter Value of A:";
+	cin>> a;
+	cout<<"Enter Value of B:";
+	cin>> b;
+	cout<<"Addition of a&b is: "<<a+b<<endl;
+	cout<<"Subraction is: "<<a-b<<endl;
+	cout<<"Multiplication is: "<<a*b<<endl;
+	cout<<"Division is: "<<a/b<<endl;
+}

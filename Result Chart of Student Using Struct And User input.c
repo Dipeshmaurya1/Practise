30eabe516addfa483a3535713225783a4fdf339c
Dipesh Maurya <149373441+Dipeshmaurya1@users.noreply.github.com>
2024-01-27 09:57:19 +0530

    Add files via upload

---
+++ Result Chart of Student Using Struct And User input.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<string.h>
+
+
+struct Student 
+{
+    int rollno;
+    char name[50];
+    int chemistry;
+    int maths;
+    int physics;
+};
+
+int main() 
+{
+    
+    int n;
+    printf("Enter The Numbber of Student: ");
+    scanf("%d",&n);
+    
+    struct Student students[n];
+
+    int i;
+    
+    for (i = 0; i < n; i++) 
+	{
+        printf("Enter details for Student %d:\n", i + 1);
+        
+       
+        printf("Roll No: ");
+        scanf("%d", &students[i].rollno);
+
+        printf("Name: ");
+        scanf("%s", students[i].name); 
+
+     
+        printf("Chemistry Marks (out of 100): ");
+        scanf("%d", &students[i].chemistry);
+
+        printf("Mathematics Marks (out of 100): ");
+        scanf("%d", &students[i].maths);
+
+        printf("Physics Marks (out of 100): ");
+        scanf("%d", &students[i].physics);
+
+        printf("\n");
+    }
+
+ 
+    printf("Mark Sheets:\n");
+    for (i = 0; i < n; i++) 
+	{
+        float total_marks = students[i].chemistry + students[i].maths + students[i].physics;
+        float percentage = (total_marks / 300) * 100;
+
+        printf("\nStudent %d - Roll No: %d\n", i + 1, students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Chemistry Marks: %d\n", students[i]. chemistry);
+        printf("Mathematics Marks: %d\n", students[i].maths);
+        printf("Physics Marks: %d\n", students[i]. physics);
+        printf("Percentage: %.2f%%\n", percentage);
+    }
+
+    return 0;
+}

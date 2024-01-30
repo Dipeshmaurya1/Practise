72982024379c30ea9abf44f20ec1c76ffbc6a8de
Dipesh Maurya <149373441+Dipeshmaurya1@users.noreply.github.com>
2024-01-30 14:00:07 +0530

    Update HH MM SS.cpp

--- HH MM SS.cpp
+++ HH MM SS.cpp
@@ -1,18 +1,28 @@
 #include<iostream>
 using namespace std;
 
+class Seconds
+{
+	public:
+	int h,m,s,rs;
+	void input()
+	{
+		cout<<"Enter Seconds: ";
+		cin>> s;
+	}
+	void output()
+	{
+		h= s/3600;
+		m=(s%3600)/60;
+		rs=s%60;
+		cout<<"The Time is=  "<<h<<":"<<m<<":"<<rs<<endl;
+	}
+};
 int main()
 {
-	int second,minutes,hours,remain_seconds;
+	Seconds s1;
+	s1.input();
+	s1.output();
 	
-	cout<<"Enter Number of Seconds: ";
-	cin>> second;
-	if(second<0)
-	{
-		cout<<"Please Enter Second more than 0 !!";
-	}
-	hours= second/3600;
-	minutes=(second%3600)/60;
-	remain_seconds=second%60;
-	cout<<"The Time is=  "<<hours<<":"<<minutes<<":"<<remain_seconds<<endl;
+	return 0;
 }
